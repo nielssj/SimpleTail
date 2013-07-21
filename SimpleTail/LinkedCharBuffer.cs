76182ad67dd85734f45c1a067895c109af5717dc
Niels Soeholm <niels@it-kartellet.dk>
2013-07-21 22:54:37 +0200

    The project now has a simple enumerable collection that can hold char array buffers in a linked list and offer an enumerator that enumerates the characters as if it was a single buffer.

---
+++ SimpleTail/LinkedCharBuffer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleTail
+{
+    public class LinkedCharBuffer : IEnumerable
+    {
+        private LinkedList<char[]> buffers;
+
+        public LinkedCharBuffer()
+        {
+            buffers = new LinkedList<char[]>();
+        }
+
+        public void addBuffer(char[] buffer)
+        {
+            this.buffers.AddFirst(buffer);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new LinkedCharBufferEnumerator(buffers);
+        }
+    }
+
+    public class LinkedCharBufferEnumerator : IEnumerator
+    {
+        private int i;
+        private LinkedList<char[]> buffers;
+        private LinkedList<char[]> visitedBuffers;
+        private char[] currentBuffer;
+
+        public LinkedCharBufferEnumerator(LinkedList<char[]> buffers)
+        {
+            i = 0;
+            this.buffers = buffers;
+            visitedBuffers = new LinkedList<char[]>();
+            currentBuffer = buffers.First.Value;
+            buffers.RemoveFirst();
+            visitedBuffers.AddLast(currentBuffer);
+        }
+
+        public object Current
+        {
+            get
+            {
+                return currentBuffer[i];
+            }
+        }
+
+        public bool MoveNext()
+        {
+            i++;
+
+            if (i < currentBuffer.Length)
+            {
+                return true;
+            }
+            else if (buffers.Count > 0)
+            {
+                currentBuffer = buffers.First.Value;
+                buffers.RemoveFirst();
+                visitedBuffers.AddLast(currentBuffer);
+                i = 0;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Reset()
+        {
+            visitedBuffers.Concat(buffers);
+            buffers = visitedBuffers;
+            visitedBuffers = new LinkedList<char[]>();
+        }
+    }
+}
